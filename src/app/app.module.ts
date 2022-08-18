d02a6fc6383c68b674749882ed48d96fa9b8e5e5
juanma04lh <78247350+juanma04lh@users.noreply.github.com>
2022-08-18 03:19:13 -0300

    experiencia css terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { InfoComponent } from './components/info/info.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EstudiosComponent } from './components/estudios/estudios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    InfoComponent
+    InfoComponent,
+    ExperienciaComponent,
+    EstudiosComponent
   ],
   imports: [
     BrowserModule,
